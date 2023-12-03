c33159798503b8da6f2396ab21675229018bd31a
kamrul islam <mdkamrulislam9810@gmail.com>
2023-12-03 14:50:55 +0600

    add-p1

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='h-screen w-screen bg-gradient-to-br from-transparent to-cyan-300'>
+      <h1>Contact with us</h1>
+
+
+      <form action="" className='bg-white w-[90%] sm:w-[500px] mx-auto rounded shadow p-10'>
+        <div className="name flex flex-col">
+          <label htmlFor="name">Name: </label>
+          <input className='border rounded-sm border-indigo-500 px-2 py-1 text-[#333] outline-none' placeholder="" type="text" name="" id="" />
+        </div>
+        <div className="name flex flex-col">
+          <label htmlFor="name">Email: </label>
+          <input className='border rounded-sm border-indigo-500 px-2 py-1 text-[#333] outline-none' placeholder="" type="email" name="" id="" />
+        </div>
+      </form>
+
+    </div>
   )
 }
 
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import vpn from '/images/vpn.png'
 
 const Project = () => {
   return (
-    <div>Project</div>
+    <div className='bg-indigo-200 min-h-screen w-full p-10'>
+
+
+      <div className="project-1 bg-white border rounded border-indigo-400 p-4">
+        <h3 className='text-left text-blue-500'>#Project: 1</h3>
+        <h1 className='text-3xl font-bold text-center border-b-2'>VPN Configuration</h1>
+        <p className='text-sm text-[#333] mt-4'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae beatae veritatis dolorum, animi officia porro neque nihil quisquam fugit, debitis velit blanditiis doloribus dignissimos repellendus at. Magnam soluta obcaecati, minima aut hic laborum excepturi odio quos, ratione enim asperiores eaque.</p>
+      </div>
+      <img className='w-[100%] mx-auto border my-4 border-indigo-400 rounded shadow' src={vpn} alt="" />
+
+      <div className="config bg-white border rounded border-indigo-400 p-4">
+        <p>step 1: Design a Diagram for configuration vpn.</p>
+      </div>
+
+    </div>
   )
 }
 
